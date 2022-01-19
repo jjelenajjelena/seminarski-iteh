8b5356ec05e545cbcb50985c2dd99f18e124a2d7
Tatjana1499 <bailovict@gmail.com>
2022-01-19 22:34:12 +0100

    izmena modela Vakcina

--- app/Vakcina.php
+++ app/Vakcina.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Vakcina extends Model
 {
     protected $table = 'vakcina';
+    
+
     public function prijave()
     {
         return $this->hasMany('App\Prijava', 'vakcina_id', 'id');
